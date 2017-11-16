32b314f7203070e16e1f997361d0b46b2820580d
Simon K Zheng <simonkzheng@gmail.com>
2017-11-16 18:15:36 -0500

    Set up

---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Styles/Navbar.css';
+
+class Navbar extends React.Component {
+  render(){
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-brand logo" src='/assets/myLogo.png'></div>
+
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="">Link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar;

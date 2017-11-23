6f2f921837492c962b3246dc6b988e3e57ff231b
Simon K Zheng <simonkzheng@gmail.com>
2017-11-22 21:13:54 -0500

    Nav scrolling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,51 @@
 import React from 'react';
+import $ from 'jquery';
 import './Styles/Navbar.css';
 
 class Navbar extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  handleScroll(e){
+    e.preventDefault();
+    let loc = $(e.target).data('link');
+    if(loc === "top"){
+      $('html, body').animate({
+        scrollTop: 0
+      }, 800);
+    } else {
+      $('html, body').animate({
+        scrollTop: $(loc).offset().top - 66
+      }, 800);
+    }
+  }
   render(){
+
     return (
       <div className='sticky'>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="navbar-brand logo" src='/assets/myLogo.png'></div>
+          <div className="navbar-brand logo" src='/assets/myLogo.png'  onClick={this.handleScroll} data-link="top"></div>
 
           <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
               <li className="nav-item active">
-                <a className="nav-link" href="#about">About</a>
+                <a className="nav-link" onClick={this.handleScroll} data-link="#about">About</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#portfolio">Portfolio</a>
+                <a className="nav-link" onClick={this.handleScroll} data-link="#portfolio">Portfolio</a>
               </li>
               {/* <li className="nav-item">
                 <a className="nav-link" href="">Blog</a>
               </li> */}
               <li className="nav-item">
-                <a className="nav-link" href="#contact">Contact</a>
+                <a className="nav-link" onClick={this.handleScroll} data-link="#contact">Contact</a>
               </li>
             </ul>
           </div>

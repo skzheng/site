ddfc234fedf0d51f35708c281ddfeda9368fcd96
Simon K Zheng <simonkzheng@gmail.com>
2018-01-29 14:38:14 -0500

    RWD Implemented

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './App';
+import fb from './db';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

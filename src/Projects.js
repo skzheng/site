697a85894efbff2df65f48b4c73b42d45209ab46
Simon K Zheng <simonkzheng@gmail.com>
2018-02-24 17:49:53 -0500

    Styling and Projects fix

--- src/Projects.js
+++ src/Projects.js
@@ -12,10 +12,10 @@ class Projects extends React.Component {
           <div className="randp project wow fadeIn" data-wow-duration="2s">
             <div className="description">
               <h2 className="project-title"> Rappers and Pemdas </h2>
-              <div className="line"/>
               <img className="project-pic" src="/assets/randp.png"/>
-              <p className="mainDescription">
-                Practice some PEMDAS with some rappers!
+              <p className="mainDescription project-description">
+                A math game to practice your order of operations with the help of some well known musicians.
+                Written in vanilla JS as part of the JS30 series.
               </p>
               <a className="hoverLink visitSite" href="https://rappersandpemdas.firebaseapp.com/">Visit site</a>
             </div>
@@ -24,14 +24,26 @@ class Projects extends React.Component {
         <div className="randp project wow fadeIn" data-wow-duration="2s">
           <div className="description">
             <h2 className="project-title"> The Word Clock </h2>
-            <div className="line"/>
             <img className="project-pic" src="/assets/wordclock.png"/>
-            <p className="mainDescription">
-              For the logophile who doesn't want to be late and hates numbers.
+            <p className="mainDescription project-description">
+              For the logophile that hates numbers and doesn't want to be late. 
+              Written in vanilla JS as part of the JS30 series.
+              <br/>
             </p>
             <a className="hoverLink visitSite" href="https://wordclock-826a3.firebaseapp.com/">Visit site</a>
           </div>
         </div>
+
+        {/* <div className="project wow fadeIn" data-wow-duration="2s">
+          <div className="project-description">
+            <h2 className="project-title"> ??? </h2>
+            <img className="project-pic" src=""/>
+            <p className="mainDescription">
+              For the logophile who doesn't want to be late and hates numbers.
+            </p>
+            <a className="hoverLink visitSite" href="">Visit site</a>
+          </div>
+        </div> */}
 
         </div>
 
